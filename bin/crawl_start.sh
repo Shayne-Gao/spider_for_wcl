d7af3ee556eab081ad29e1b54f1a7e20eacb9c90
Shayne Gao <gwlj@outlook.com>
2017-08-09 15:13:27 +0800

    change bin/shell

--- bin/crawl_start.sh
+++ bin/crawl_start.sh
@@ -2,5 +2,6 @@ roles=("Balance" "Guardian" "Restoration" "Brewmaster")
 ls_date=`date +%Y%m%d`
 for role in ${roles[@]}
 do
-scrapy crawl wcl -o keystone15-20_${role}_${ls_date}.csv -a role="${role}" 
+scrapy crawl wcl -o ../output/keystone15-20_${role}_${ls_date}.csv -a role="${role}" 
+cd ../processer_java/&& java Analysiser ../output/keystone15-20_${role}_${ls_date}.csv
 done
